f6bb9dbeac23c403d725a206e317c41b5c0deec6
marsett <jimenezmarset@gmail.com>
2024-10-13 12:40:19 +0200

    final

--- BibliotecaLibrosAPI/BibliotecaLibrosAPI/obj/Debug/net8.0/BibliotecaLibrosAPI.AssemblyInfo.cs
+++ BibliotecaLibrosAPI/BibliotecaLibrosAPI/obj/Debug/net8.0/BibliotecaLibrosAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BibliotecaLibrosAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f24e1fff3bc66bd03b2080579d6ddfea5e82c89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8864ad63b933cef32a37d7ba2deb336a35c7d68")]
 [assembly: System.Reflection.AssemblyProductAttribute("BibliotecaLibrosAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BibliotecaLibrosAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
